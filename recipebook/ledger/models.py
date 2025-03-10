2f95d34ed1f8bfe8d4abf6bca445f14e48df1486
bea-ndc <beatrice.decastro@student.ateneo.edu>
2025-03-10 22:49:23 +0800

    Updated the author field for Recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -18,12 +18,7 @@ class Recipe(models.Model):
     name = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now_add=True, null=True)
     updated_on = models.DateTimeField(auto_now=True, null=True)
-    author = models.ForeignKey(
-        Profile,
-        on_delete=models.SET_NULL,
-        null=True,
-        related_name='author'
-    )
+    author = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return '{}'.format(self.name)
