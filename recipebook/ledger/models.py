fa1ef2d6202bc852f04ab192272ddba005c8d62f
bea-ndc <beatrice.decastro@student.ateneo.edu>
2025-03-10 21:12:21 +0800

    Added more fields for the Recipe model
    Added created_on, updated_on, and author fields to the Recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.urls import reverse
 
+from accounts.models import Profile
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
-
     def __str__(self):
         return '{}'.format(self.name)
-    
 
     def get_absolute_url(self):
         return reverse('intgredient_detail', args=[str(self.name)])
@@ -16,11 +16,17 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, null=True)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='author'
+    )
 
     def __str__(self):
         return '{}'.format(self.name)
-    
 
     def get_absolute_url(self):
         return reverse('ledger:recipe_detail', args=[str(self.pk)])
@@ -29,14 +35,15 @@ class Recipe(models.Model):
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
     ingredient = models.ForeignKey(
-        Ingredient, 
+        Ingredient,
         on_delete=models.CASCADE,
         related_name="recipe"
-        )
+    )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
         related_name="ingredients"
-        ) 
+    )
+
 
 # Create your models here.
