c5ac586a23645adf3cd21273de1005014364dd11
bea-ndc <beatrice.decastro@student.ateneo.edu>
2025-03-24 21:55:06 +0800

    Updated ledger views
    Added CreateViews for RecipeImage and for Recipe

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse, reverse_lazy
 
-
-from .models import Recipe
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageUploadForm
 
 
 class RecipeListView(LoginRequiredMixin, ListView):
@@ -18,6 +20,43 @@ class RecipeDetailView(DetailView):
     model = Recipe
     template_name = 'recipe_detail.html'
     redirect_field_name = 'login'
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'recipe_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('ledger:recipe_detail', kwargs={'pk': self.object.pk})
+
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    form_class = RecipeImageUploadForm
+    template_name = 'recipe_imageform.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        pk = self.kwargs['pk']
+        ctx['pk'] = pk
+        ctx['form'] = RecipeImageUploadForm()
+        return ctx
+
+    def post(self, request, *args, **kwargs):
+        pk = self.kwargs['pk']
+        form = RecipeImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe_image = RecipeImage()
+            recipe_image.image = request.FILES.get('image')
+            recipe_image.recipe = Recipe.objects.get(pk=pk)
+            recipe_image.save()
+            return redirect(reverse('ledger:recipe_detail', args=[pk]))
+        else:
+            self.object_list = self.get_queryset(**kwargs)
+            context = self.get_context_data(**kwargs)
+            context['form'] = form
+            return self.render_to_response(context)
 
 
 # Create your views here.
--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings 
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ledger.urls', namespace = 'ledger')),
-    #path('', include('accounts.urls', namespace = 'accounts')),
+    path('', include('ledger.urls', namespace='ledger')),
+    # path('', include('accounts.urls', namespace = 'accounts')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
