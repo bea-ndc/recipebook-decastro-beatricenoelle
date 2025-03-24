795a872bb33b7a7fa08439fa56c1b294ddbbc1cc
bea-ndc <beatrice.decastro@student.ateneo.edu>
2025-03-24 21:41:34 +0800

    Added the RecipeImage model
    Added the RecipeImage model and necessary fields and foreign keys

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -41,4 +41,15 @@ class RecipeIngredient(models.Model):
     )
 
 
+class RecipeImage(models.Model):
+    image = models.ImageField(upload_to='images/', null=True)
+    description = models.TextField(max_length=255)
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="recipe_image"
+    )
+
+
 # Create your models here.
