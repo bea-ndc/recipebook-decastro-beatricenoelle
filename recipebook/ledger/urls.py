979ab4ca746f44190ff79f96b7e3e1c8b01a7a92
bea-ndc <beatrice.decastro@student.ateneo.edu>
2025-03-24 21:56:04 +0800

    Updated urls for ledger

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import (
+    RecipeListView, RecipeDetailView,
+    RecipeCreateView, RecipeImageCreateView
+
+)
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='list'),
-    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail')
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipe/add', RecipeCreateView.as_view(), name='recipe-create'),
+    path('recipe/<int:pk>/add_image',
+         RecipeImageCreateView.as_view(), name='image-create'),
 ]
 
 
